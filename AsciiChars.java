cda7dc01dde29efe63369bc162c6cf1e31375031
asteph <aliciap.stephens@gmail.com>
2020-12-22 01:36:39 +0000

    move

---
+++ AsciiChars.java
@@ -0,0 +1,51 @@
+
+public class AsciiChars {
+
+    public static void main(String[] args) {
+         public static void printNumbers() 
+        for (int x = 0; x <128; x++) {
+            System.out.println(x);
+        }
+//capital letters
+{ public static void printUpperCase()
+    for (int x = 65; x <91; x++) {
+            System.out.print(x);
+            System.out.prinlin("\t" + (char)x);
+    }
+}     
+// lowercase
+public static void printLowerCase() {
+                for (int x = 97 x <123; x++) {
+            System.out.print(x);
+            System.out.prinlin("\t" + (char)x);
+        }
+        }
+
+	
+	printNumbers();
+	printLowerCase();
+	printUpperCase();
+	}
+ public static void printNumbers() {
+	 int i;
+	 System.out.println("Numbers are: \n");
+	 for (i = 48; i < 58; i++) {
+	 System.out.println((char)i);
+	 System.out.print("\n");
+	 }
+ } public static void printLowerCase() {
+	 int i;
+	 System.out.println("Lower case Alphabets are: \n");
+	 for (i = 97; i < 122; i++) {
+	 System.out.println((char)i);
+	 System.out.print("\n");
+	 }
+ } public static void printUpperCase() {
+	 int i;
+	 System.out.println("Upper case Alphabets are: \n");
+	 for (i = 65; i < 90; i++) {
+	 System.out.println((char)i);
+	 System.out.print("\n");
+	 }
+	 }
+ }
